60bf5a9cdcc8c12d4a7b6db888707e955ce6a222
KOSASIH <kosasihg88@gmail.com>
2023-10-12 15:48:18 +0700

    Create OIDCStrategy.py

---
+++ OIDCStrategy.py
@@ -0,0 +1,73 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const OIDCStrategy = require('passport-openidconnect').Strategy;
+
+const app = express();
+
+// Configure session middleware
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Configure passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure OIDC strategy
+passport.use(new OIDCStrategy({
+  issuer: 'https://your-oidc-provider.com',
+  authorizationURL: 'https://your-oidc-provider.com/authorize',
+  tokenURL: 'https://your-oidc-provider.com/token',
+  userInfoURL: 'https://your-oidc-provider.com/userinfo',
+  clientID: 'your-client-id',
+  clientSecret: 'your-client-secret',
+  callbackURL: 'http://localhost:3000/callback',
+  scope: 'openid profile email'
+}, (accessToken, refreshToken, profile, done) => {
+  // Save the access token and ID token to the session
+  req.session.accessToken = accessToken;
+  req.session.idToken = profile.id;
+
+  // Handle user authentication logic here
+
+  return done(null, profile);
+}));
+
+// Configure passport serialization
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/login', passport.authenticate('openidconnect'));
+
+app.get('/callback', passport.authenticate('openidconnect', {
+  successRedirect: '/profile',
+  failureRedirect: '/login'
+}));
+
+app.get('/profile', (req, res) => {
+  // Access the access token and ID token from the session
+  const accessToken = req.session.accessToken;
+  const idToken = req.session.idToken;
+
+  // Use the tokens to access user information or perform other actions
+
+  res.send('User profile');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
